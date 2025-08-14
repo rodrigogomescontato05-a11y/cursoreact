14bc42d9c16b697b2a9922833836e4a3007f9e4d
gomesrodrigo <rodrigogomes.contato05@gmail.com>
2025-08-14 18:37:19 +0000

    Conceito de Classe

--- 1_components/src/App.jsx
+++ 1_components/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
+import ClassComponent from './components/ClassComponent'
 import FunctionalComponent from './components/FunctionalComponent'
 
 function App() {
 
   return (
     <>
-     <h1>Hello World</h1>
-     <FunctionalComponent/>
+      <h1>Hello World</h1>
+      <FunctionalComponent/>
+      <ClassComponent/>
     </>
   )
 }

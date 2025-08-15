5dc8d18f277cb0009bdd3536fd93f9572418dd68
gomesrodrigo <rodrigogomes.contato05@gmail.com>
2025-08-15 17:59:49 +0000

    Projeto Todo

---
+++ todoapp/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "criar funcionalidade",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+        {
+      id: 2,
+      text: "ir para academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+        {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    },
+  ])
+  return (
+    <>
+      <div className="app">
+        <h1>Lista de Tarefas</h1>
+        <div className="todo-list">
+          {todos.map((todo) => (
+            <div className="todo">
+              <div className="content">
+                <p>{todo.text}</p>
+                <p>{todo.category}</p>
+              </div>
+              <div>
+                <button>Completar</button>
+                <button> X </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
